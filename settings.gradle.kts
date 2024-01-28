27f36c8b63151486f97b1fcf7464bfa5f91f3214
Ricardo Sousa <sousa.ricardorodrigues@gmail.com>
2024-01-28 15:01:55 +0100

    config: adding first modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,12 @@ dependencyResolutionManagement {
 
 rootProject.name = "CaloryTracker"
 include(":app")
- 
+include(":core")
+include(":onboarding")
+include(":onboarding:onboarding_presentation")
+include(":onboarding:onboarding_domain")
+include(":onboarding:onboarding_data")
+include(":tracker")
+include(":tracker:tracker_presentation")
+include(":tracker:tracker_domain")
+include(":tracker:tracker_data")
