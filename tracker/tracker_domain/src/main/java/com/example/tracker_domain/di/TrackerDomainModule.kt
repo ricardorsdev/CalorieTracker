dfa8c08fbc0a5a92c8b59a566514ba0912fb549e
Ricardo Sousa <sousa.ricardorodrigues@gmail.com>
2024-02-05 09:29:45 +0100

    feat: defining Use Cases

---
+++ tracker/tracker_domain/src/main/java/com/example/tracker_domain/di/TrackerDomainModule.kt
@@ -0,0 +1,35 @@
+package com.example.tracker_domain.di
+
+import com.example.core_domain.preferences.Preferences
+import com.example.tracker_domain.repository.TrackerRepository
+import com.example.tracker_domain.use_case.CalculateMealNutrients
+import com.example.tracker_domain.use_case.DeleteTrackedFood
+import com.example.tracker_domain.use_case.GetFoodsForDate
+import com.example.tracker_domain.use_case.SearchFood
+import com.example.tracker_domain.use_case.TrackFood
+import com.example.tracker_domain.use_case.TrackerUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object TrackerDomainModule {
+
+    @Provides
+    @Singleton
+    fun provideTrackerUseCases(
+        repository: TrackerRepository,
+        preferences: Preferences
+    ): TrackerUseCases {
+        return TrackerUseCases(
+            calculateMealNutrients = CalculateMealNutrients(preferences),
+            deleteTrackedFood = DeleteTrackedFood(repository),
+            getFoodsForDate = GetFoodsForDate(repository),
+            searchFood = SearchFood(repository),
+            trackFood = TrackFood(repository),
+        )
+    }
+}
