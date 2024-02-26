75f86e4172adc94d773dd2dff73f61fc223222ff
Ricardo Sousa <sousa.ricardorodrigues@gmail.com>
2024-02-26 22:34:16 +0100

    feat: Unit Testing Use Case

--- tracker/tracker_data/build.gradle.kts
+++ tracker/tracker_data/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation(Retrofit.okHttp)
     implementation(Retrofit.okHttpLoggingInterceptor)
     implementation(Retrofit.moshiConverter)
+    testImplementation("junit:junit:4.12")
 
     "kapt"(Room.roomCompiler)
     implementation(Room.roomKtx)
